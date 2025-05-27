c90ddd06c17d8f8168b240fd9106c58189407fe0
SeokJune <82258848+EarthRabbit@users.noreply.github.com>
2025-05-27 21:41:17 +0900

    [Bronze III] Title: 전자레인지, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.cc
+++ 백준/Bronze/10162. 전자레인지/전자레인지.cc
@@ -3,16 +3,15 @@ using namespace std;
 
 int main() {
 
-    int T, temp = 0, sum = 0;
+    int T, sum = 0;
     int timeSet[3] = {300, 60, 10};
     int timeTime[3];
     
     cin >> T;
     
-    while(T > 9){
-        timeTime[temp] = (T / timeSet[temp]);
-        T = T % timeSet[temp];
-        temp++;
+    for(int i = 0; i < 3; i++){
+        timeTime[i] = (T / timeSet[i]);
+        T = T % timeSet[i];
     }
 
     if(T == 0){
