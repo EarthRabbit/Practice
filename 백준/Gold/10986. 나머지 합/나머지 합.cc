210bf72549579d2253b430f055c7f393e121f47f
SeokJune <82258848+EarthRabbit@users.noreply.github.com>
2025-07-05 15:41:20 +0900

    [Gold III] Title: 나머지 합, Time: 108 ms, Memory: 5812 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -11,29 +11,21 @@ int main() {
     int N, M, temp = 0;
     unsigned long long int sum = 0, cnt = 0;
     int num[1000001];
-    vector<int> remainder;
-    remainder.push_back(0);
-    
+    unsigned long long int remainder[1001];
+    remainder[0]++;
+  
     cin >> N >> M;
 
     for(int i = 1; i <= N; i++){
         cin >> num[i];
-        remainder.push_back((remainder[i - 1] + num[i]) % M);
+        temp = (temp + num[i]) % M;
+        remainder[temp]++;
     }
 
-    sort(remainder.begin(), remainder.end());
-
-    for(int i = 0; i <= N; i++){
-        if(temp == remainder[i]) cnt++;
-        else{
-            sum += (cnt * (cnt - 1)) / 2;
-            cnt = 1;
-            temp = remainder[i];
-        }
+    for(int i = 0; i <= M; i++){
+        sum += (remainder[i] * (remainder[i] - 1)) / 2;
     }
-
-    sum += (cnt * (cnt - 1)) / 2;
-
+    
     cout << sum;
     
     return 0;
