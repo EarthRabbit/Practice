ffdd7f7c6eeac8462bb0d917d18f2bdd8690b24b
SeokJune <82258848+EarthRabbit@users.noreply.github.com>
2025-07-05 15:45:49 +0900

    [Gold III] Title: 나머지 합, Time: 108 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -8,17 +8,16 @@ int main() {
     cin.tie(0);
     ios::sync_with_stdio(false);
     
-    int N, M, temp = 0;
-    unsigned long long int sum = 0, cnt = 0;
-    int num[1000001];
-    unsigned long long int remainder[1001];
+    int N, M, temp = 0, num;
+    unsigned long long int sum = 0;
+    unsigned long long int remainder[1001] = {0};
     remainder[0]++;
   
     cin >> N >> M;
 
     for(int i = 1; i <= N; i++){
-        cin >> num[i];
-        temp = (temp + num[i]) % M;
+        cin >> num;
+        temp = (temp + num) % M;
         remainder[temp]++;
     }
 
